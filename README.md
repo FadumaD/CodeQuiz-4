bf45edb7b44f454ecee5901856b8a8a0cacaec35
Faduma Dahir <fadumadahir@Fadumas-MacBook-Air.local>
2023-04-26 21:34:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 04 Web APIs: Code Quiz
+
+
+Project Name - timed quiz 
+
+Project Description:
+This challanged is a timed quiz that has multiplie choice question to answer with in the time slot given. The answer given will then be marked as correct or incorrect
+
+This project 
+
+How to Install and Run the Project:
+
+This project can be cloned from my repository via the git hub website by following the link
+
+and can be cloned to your personal repo.
+
+
+Website image: # Challenge4-Quiz
+# CodeQuiz-4
---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'What is used to change the look of a page? ',
+    choices: ['SQL', 'JavaScript', 'HTML', 'CSS'],
+    answer: 'CSS',
+  },
+  {
+    title: 'Which of the following HTML tags would result in bold text?',
+    choices: ["< bold >", "< em >", "< br >", "< strong >"],
+    answer: '<strong>',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'Which CSS property controls text size?',
+    choices: ["font-size", "text-size", "font-style", "size"],
+    answer: 'Font-size',
+  },
+];
